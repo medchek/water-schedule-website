e6294ce951b9ffa23f07158ab4b63d5c47653f0a
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-09 23:33:07 +0100

    feat: add title to the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import AboutHero from "../components/about/AboutHero";
 import MoreProjects from "../components/about/MoreProjects";
 import ProjectDetail from "../components/about/ProjectDetail";
@@ -8,6 +9,14 @@ import Footer from "../components/common/Footer";
 const About: NextPage = () => {
   return (
     <AppMain>
+      <Head>
+        <title>À propos - SEAAL</title>
+        <meta
+          name="description"
+          content="Site web de l'application des programmes d'eau de seaal"
+        />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <div className="bg-white dark:bg-darkAppBg">
         <AboutHero />
         <main>
